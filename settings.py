a3f5879d64524205ba8ea7cf3405d81113e75cbf
Tom O'Brien <thomas.obrien86@gmail.com>
2017-11-21 21:37:40 +0000

    Add init and settings

---
+++ settings.py
@@ -0,0 +1,6 @@
+import os
+
+
+BITSTAMP_CUSTOMER_ID = os.environ.get('BITSTAMP_CUSTOMER_ID', '')
+BITSTAMP_API_KEY = os.environ.get('BITSTAMP_API_KEY', '')
+BITSTAMP_API_SECRET = os.environ.get('BITSTAMP_API_SECRET', '')
